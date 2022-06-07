e0015cb41c0ac7fd5473ea2ba83a9493b73d0656
9659tig <tig9659@ajou.ac.kr>
2022-06-07 11:15:48 +0900

    api 작업 중

--- src/routers/main.js
+++ src/routers/main.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect} from 'react';
+//import Post from '../components/feed'
+import axios from 'axios';
 
-function Main(props){
+function Main(){
+    const [posts, setPosts] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        const fetchPosts = async () => {
+            try{
+                setError(null);
+                setPosts(null);
+                setLoading(true);
+                const response = await axios.get('http://localhost:5000/feed');
+                setPosts(response.data);
+            } catch(e){
+                setError(e);
+            }
+            setLoading(false);
+        }
+        fetchPosts();
+    },[])
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error!!</div>;
+    if (!posts) return null;
+
     return(
-        <>
-            <h2>Main</h2>
-        </>
+        <ul>
+            {posts && posts.result.map(post =>(
+                <li key={post.feedIdx}>
+                    {post.title}
+                </li>
+            ))}
+        </ul>
     )
+    // return(
+    //     <div>Main</div>
+    // )
+
 }
 
 export default Main;
